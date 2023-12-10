d6cdea5184507c24b6b7abca7d55161acc984182
epileptickeed <101656721+epileptickeed@users.noreply.github.com>
2023-12-10 12:43:13 +0300

    Add files via upload

--- interactive comment section/assets/script/script.js
+++ interactive comment section/assets/script/script.js
@@ -8,6 +8,8 @@ let afterBlock = document.querySelector('.main .after_block')
 let personName = document.querySelectorAll('.name')
 
 
+
+
 let scoreValue = 0
 voteScore.forEach(vote=>{
     vote.innerHTML = `${scoreValue}`
@@ -31,139 +33,152 @@ downvote.forEach(down=>{
     })
 })
 
+// reply.forEach(replie =>{
+//     replie.addEventListener('click', newComment)
+// })
+
+
 reply.forEach(replie =>{
-    replie.addEventListener('click', newComment)
+    replie.addEventListener('click', (e)=>{
+        let newDiv = document.createElement('div')
+        newDiv.className = 'comment_block'
+        newDiv.style.marginLeft = '5%'
+
+        //VOTES
+        let voteDiv = document.createElement('div')
+        voteDiv.className = 'votes'
+        voteDiv.style.display = 'none'
+
+        let upvoteSpan = document.createElement('span')
+        let upvoteContext = document.createTextNode('+')
+        upvoteSpan.appendChild(upvoteContext)
+        upvoteSpan.className = 'upvote'
+
+        let voteScoreSpan = document.createElement('span')
+        let voteScoreContext = document.createTextNode('0')
+        voteScoreSpan.appendChild(voteScoreContext)
+        voteScoreSpan.className = 'vote_score'
+
+        let downvoteSpan = document.createElement('span')
+        let downvoteSpanContext = document.createTextNode('-')
+        downvoteSpan.appendChild(downvoteSpanContext)
+        downvoteSpan.className = 'downvote'
+
+        //INFO_BLOCK
+        let infoBlock = document.createElement('div')
+        infoBlock.className = 'info_block'
+
+        let infoBlockInner = document.createElement('div')
+        infoBlockInner.className = 'info_block_inner'
+
+        let detailsBlock = document.createElement('div')
+        detailsBlock.className = 'details'
+        
+        let avatar = document.createElement('img')
+        avatar.src = 'assets/images/avatars/image-juliusomo.png'
+        avatar.className = 'avaImage'
+        
+        let name = document.createElement('p')
+        let nickName = document.createTextNode('juliosomo')
+        name.appendChild(nickName)
+        name.className = 'name'
+
+        let dateNow = new Date();
+        let minutes = dateNow.getMinutes();
+        let hours = dateNow.getHours()
+        let month = dateNow.getMonth()
+        let days = dateNow.getDay()
+        let years = dateNow.getFullYear()
+        let date5SecondsAgo = (`at ${hours}:${minutes} on ${days}/${month}/${years}`);
+
+        let date = document.createElement('p')
+        let currentDate = document.createTextNode(date5SecondsAgo)
+        date.appendChild(currentDate)
+        date.className = 'date'
+
+        let deleteBtn = document.createElement('p')
+        let deleteText = document.createTextNode('Delete')
+        deleteBtn.appendChild(deleteText)
+        deleteBtn.className = 'deleteBtn'
+
+        let editBtn = document.createElement('p')
+        let editText = document.createTextNode('Edit')
+        editBtn.appendChild(editText)
+        editBtn.className = 'editBtn'
+
+        let blockText = document.createElement('p')
+        let blockTextT = document.createTextNode('')
+        blockText.appendChild(blockTextT)
+        blockText.className = 'block_text'
+
+        let textSubDiv = document.createElement('div')
+        textSubDiv.className = 'textSubDiv'
+
+        let textArea = document.createElement('textarea')
+        // let areaNick = document.createTextNode(personName.forEach(name=>{
+        //     document.querySelector(personName)?.console.log(name)
+        // }))
+        // textArea.appendChild(areaNick)
+        textArea.className = 'textArea'
+
+        let submitBtn = document.createElement('input')
+        submitBtn.className = 'submitBtn'
+        submitBtn.type = 'submit'
+
+        //ADD TO THE DIV
+        
+        function insertAfter(newNode, existingNode) {
+            existingNode.parentNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+        }
+
+        insertAfter(newDiv, e.target)
+
+        // const currentDiv = document.querySelector(".aria_block");
+        // main.insertBefore(newDiv, comBlock.forEach(block=>{
+        //     block.value - 1
+        //     // console.log(this.personName)
+        // }));
+        //append VOTES
+        newDiv.appendChild(voteDiv)
+        voteDiv.appendChild(upvoteSpan)
+        voteDiv.appendChild(voteScoreSpan)
+        voteDiv.appendChild(downvoteSpan)
+
+        //append INFO_BLOCK 
+        newDiv.appendChild(infoBlock)
+            infoBlock.appendChild(infoBlockInner)
+                infoBlockInner.appendChild(detailsBlock)
+                    detailsBlock.appendChild(avatar)
+                    detailsBlock.appendChild(name)
+                    detailsBlock.appendChild(date)
+                infoBlockInner.appendChild(deleteBtn)
+                infoBlockInner.appendChild(editBtn)
+            infoBlock.appendChild(textSubDiv)
+                textSubDiv.appendChild(textArea)
+                textSubDiv.appendChild(submitBtn)
+
+
+        submitBtn.addEventListener('click', (event)=>{
+            voteDiv.style.display = 'flex'   
+
+            blockText.value = `${textArea.value}`
+            infoBlock.removeChild(textSubDiv)
+            infoBlock.appendChild(blockText)
+            blockText.innerHTML = `${blockText.value}`
+
+        })
+
+        deleteBtn.addEventListener('click', ()=>{
+            newDiv.remove()
+        })
+
+        editBtn.addEventListener('click', ()=>{
+            infoBlock.appendChild(textSubDiv)
+            infoBlock.removeChild(blockText)
+        })
+    })
 })
 
 function newComment(){
-    let newDiv = document.createElement('div')
-    newDiv.className = 'comment_block'
-    newDiv.style.marginLeft = '5%'
-
-    //VOTES
-    let voteDiv = document.createElement('div')
-    voteDiv.className = 'votes'
-    voteDiv.style.display = 'none'
-
-    let upvoteSpan = document.createElement('span')
-    let upvoteContext = document.createTextNode('+')
-    upvoteSpan.appendChild(upvoteContext)
-    upvoteSpan.className = 'upvote'
-
-    let voteScoreSpan = document.createElement('span')
-    let voteScoreContext = document.createTextNode('0')
-    voteScoreSpan.appendChild(voteScoreContext)
-    voteScoreSpan.className = 'vote_score'
-
-    let downvoteSpan = document.createElement('span')
-    let downvoteSpanContext = document.createTextNode('-')
-    downvoteSpan.appendChild(downvoteSpanContext)
-    downvoteSpan.className = 'downvote'
-
-    //INFO_BLOCK
-    let infoBlock = document.createElement('div')
-    infoBlock.className = 'info_block'
-
-    let infoBlockInner = document.createElement('div')
-    infoBlockInner.className = 'info_block_inner'
-
-    let detailsBlock = document.createElement('div')
-    detailsBlock.className = 'details'
     
-    let avatar = document.createElement('img')
-    avatar.src = 'assets/images/avatars/image-juliusomo.png'
-    avatar.className = 'avaImage'
-    
-    let name = document.createElement('p')
-    let nickName = document.createTextNode('juliosomo')
-    name.appendChild(nickName)
-    name.className = 'name'
-
-    let dateNow = new Date();
-    let minutes = dateNow.getMinutes();
-    let hours = dateNow.getHours()
-    let month = dateNow.getMonth()
-    let days = dateNow.getDay()
-    let years = dateNow.getFullYear()
-    let date5SecondsAgo = (`at ${hours}:${minutes} on ${days}/${month}/${years}`);
-
-    let date = document.createElement('p')
-    let currentDate = document.createTextNode(date5SecondsAgo)
-    date.appendChild(currentDate)
-    date.className = 'date'
-
-    let deleteBtn = document.createElement('p')
-    let deleteText = document.createTextNode('Delete')
-    deleteBtn.appendChild(deleteText)
-    deleteBtn.className = 'deleteBtn'
-
-    let editBtn = document.createElement('p')
-    let editText = document.createTextNode('Edit')
-    editBtn.appendChild(editText)
-    editBtn.className = 'editBtn'
-
-    let blockText = document.createElement('p')
-    let blockTextT = document.createTextNode('')
-    blockText.appendChild(blockTextT)
-    blockText.className = 'block_text'
-
-    let textSubDiv = document.createElement('div')
-    textSubDiv.className = 'textSubDiv'
-
-    let textArea = document.createElement('textarea')
-    // let areaNick = document.createTextNode(personName.forEach(name=>{
-    //     document.querySelector(personName)?.console.log(name)
-    // }))
-    // textArea.appendChild(areaNick)
-    textArea.className = 'textArea'
-
-    let submitBtn = document.createElement('input')
-    submitBtn.className = 'submitBtn'
-    submitBtn.type = 'submit'
-
-    //ADD TO THE DIV
-    
-    const currentDiv = document.querySelector(".aria_block");
-    main.insertBefore(newDiv, comBlock.forEach(block=>{
-        block.value - 1
-        // console.log(this.personName)
-    }));
-    //append VOTES
-    newDiv.appendChild(voteDiv)
-    voteDiv.appendChild(upvoteSpan)
-    voteDiv.appendChild(voteScoreSpan)
-    voteDiv.appendChild(downvoteSpan)
-
-    //append INFO_BLOCK 
-    newDiv.appendChild(infoBlock)
-        infoBlock.appendChild(infoBlockInner)
-            infoBlockInner.appendChild(detailsBlock)
-                detailsBlock.appendChild(avatar)
-                detailsBlock.appendChild(name)
-                detailsBlock.appendChild(date)
-            infoBlockInner.appendChild(deleteBtn)
-            infoBlockInner.appendChild(editBtn)
-        infoBlock.appendChild(textSubDiv)
-            textSubDiv.appendChild(textArea)
-            textSubDiv.appendChild(submitBtn)
-
-
-    submitBtn.addEventListener('click', (event)=>{
-        voteDiv.style.display = 'flex'   
-
-        blockText.value = `${textArea.value}`
-        infoBlock.removeChild(textSubDiv)
-        infoBlock.appendChild(blockText)
-        blockText.innerHTML = `${blockText.value}`
-
-    })
-
-    deleteBtn.addEventListener('click', ()=>{
-        newDiv.remove()
-    })
-
-    editBtn.addEventListener('click', ()=>{
-        infoBlock.appendChild(textSubDiv)
-        infoBlock.removeChild(blockText)
-    })
 }
