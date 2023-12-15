d69c932c6065245d17909b1967b7ce27a653b733
epileptickeed <101656721+epileptickeed@users.noreply.github.com>
2023-12-15 14:48:15 +0300

    Delete ecomerce product directory

--- ecomerce product/assets/script/script.js
+++
@@ -1,9 +0,0 @@
-let counter = document.querySelector('.counter p')
-
-document.querySelectorAll('.counter img').forEach(img=>{
-    img.addEventListener('click', (e)=>{
-        e.preventDefault()
-        e.target === document.querySelector('.minus') ? counter.innerHTML = `${+counter.innerHTML - 1}` : counter.innerHTML = `${+counter.innerHTML + 1}`
-    })
-    
-})
